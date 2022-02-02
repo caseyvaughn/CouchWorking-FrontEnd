7f46662bc4602c6758c6f81287f43fa39d931646
timhausweiler <haustimp@gmail.com>
2022-02-02 15:47:21 -0500

    complete CreateCouch component

--- couchworking-app/src/App.js
+++ couchworking-app/src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom"
 import CouchList from "./components/CouchList/CouchList";
 
 import './App.css';
+import CreateCouch from "./screens/CouchCreate/CreateCouch";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path = "/couches" element={<CouchList/>}/>
+        <Route path = "/create" element={<CreateCouch/>}/>
       </Routes>
     </div>
   );
