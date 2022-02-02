26456cd4a65007e1c56365532ab177e920170fa2
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-02-02 16:12:49 -0500

    mapping over couches

--- couchworking-app/src/components/CouchList/CouchList.jsx
+++ couchworking-app/src/components/CouchList/CouchList.jsx
@@ -4,16 +4,35 @@ import { useState, useEffect } from "react"
 const baseURL = "https://couch-working.herokuapp.com/"
 
 
-export default function CouchList() {
-  const [couch, setCouch] = useState([])
+export default function Couchh3st() {
+  const [couches, setCouches] = useState([])
 
   useEffect(() => {
     const fetchCouches = async () => {
       const res = await axios.get(`${baseURL}couch-api/couches`)
       console.log(res.data)
+      setCouches(res.data)
     }
     fetchCouches();
   }, [])
-  return <div>{couch}</div>
+
+  return(
+    <div>
+      <ul>
+        {couches.map((couch) => {
+          return (
+            <li key={couch.id}> 
+              
+              <h1>{couch.title}</h1>
+              <img src={couch.image_URL} alt={couch.title}/>
+              <h3>{couch.location}</h3>
+              <p>{couch.description}</p>
+              <h5>{couch.contactInfo}</h5>   
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
 
