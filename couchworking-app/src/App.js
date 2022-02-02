b6a8977d2bc75e269e544ee8e28508268f49a25c
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-02-02 18:40:29 -0500

    able to display couch detail on page

--- couchworking-app/src/App.js
+++ couchworking-app/src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom"
 import CouchList from "./screens/CouchList/CouchList";
+import CouchDetail from "./screens/CouchDetail/CouchDetail";
 
 import './App.css';
 
@@ -7,7 +8,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path = "/couches" element={<CouchList/>}/>
+        <Route path="/couches" element={<CouchList />} />
+        <Route path = "/couch/:id" element={<CouchDetail/>}/>
       </Routes>
     </div>
   );
--- couchworking-app/src/screens/CouchDetail/CouchDetail.jsx
+++ couchworking-app/src/screens/CouchDetail/CouchDetail.jsx
@@ -1,12 +1,31 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
+import {useParams} from "react-router-dom"
 const baseURL = "https://couch-working.herokuapp.com/"
 
 export default function CouchDetail() {
-  const [couch, setCouch] = useState([])
+  const [couch, setCouch] = useState(null)
+  const { id } = useParams()
+
   useEffect(() => {
     const fetchCouch = async () => {
-      
+      const res = await axios.get(`${baseURL}couch-api/couch/${id}`)
+      console.log(res.data)
+      setCouch(res.data);
     }
-  })
+    fetchCouch();
+  }, [])
+
+
+  ///add if block for if couch does not exist
+
+  return (
+    <div>
+      <h1>{couch?.title}</h1>
+      <img src={couch?.image_URL} alt={couch?.title} />
+      <h3>{couch?.location}</h3>
+      <p>{couch?.description}</p>
+      <h5>{couch?.contactInfo}</h5>
+    </div>
+  )
 }
