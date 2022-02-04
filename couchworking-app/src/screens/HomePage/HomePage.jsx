b4eb1f1b4f4be0bbaba3cf1bcbd5b8325b9bf9fe
bweinmann <bsweinmann@gmail.com>
2022-02-04 15:23:57 -0500

    Home and Footer modified, testing button functionality

--- couchworking-app/src/screens/HomePage/HomePage.jsx
+++ couchworking-app/src/screens/HomePage/HomePage.jsx
@@ -9,7 +9,20 @@ export default function HomePage(props) {
     <div>
         <Display user={props.user}>
         <div className="home-container">
-          
+          <div className="home-container-left">
+          <button className="home-button">
+          <img className="home-image-left"
+            src = {require("../../assets/Living room1-01.jpg")}
+            alt = "Interior Modern living room with furniture. Flat design Vector Illustration Vectors by Vecteezy"></img>
+          </button>  
+         </div>
+         <div className="home-container-right">
+            <button className="home-button">
+          <img className="home-image-right"
+            src = {require("../../assets/My project.jpg")}
+            alt = "man on couch"></img>
+            </button> 
+          </div>
         </div>
         </Display>
     </div>
