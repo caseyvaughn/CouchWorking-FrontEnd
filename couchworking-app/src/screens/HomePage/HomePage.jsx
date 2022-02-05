7a5446a998178044eed93ba120f4d51987f640d9
bweinmann <bsweinmann@gmail.com>
2022-02-05 15:06:12 -0500

    right button fixed, navigation added, need to account for user status

--- couchworking-app/src/screens/HomePage/HomePage.jsx
+++ couchworking-app/src/screens/HomePage/HomePage.jsx
@@ -11,14 +11,18 @@ export default function HomePage(props) {
         <Display user={props.user}>
         <div className="home-container">
           <div className="home-container-left">
+            <Link to="/sign-in">
           <button className="home-button-left">
           Host a Couch
           </button>  
+          </Link>
          </div>
          <div className="home-container-right">
+         <Link to="/couches">
             <button className="home-button-right">
             Find a Couch
             </button> 
+          </Link>
           </div>
         </div>
         </Display>
