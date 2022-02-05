86f2c52c5b1c8fcf7ed3f2068440a36e3da3a648
bweinmann <bsweinmann@gmail.com>
2022-02-05 17:17:46 -0500

    deleted commented code

--- couchworking-app/src/components/Footer/Footer.jsx
+++ couchworking-app/src/components/Footer/Footer.jsx
@@ -42,32 +42,3 @@ const Footer = () => {
 
 export default Footer;
 
-
-{/* <div className="footer-container">
-             <h2 className="footer-start">Contact Us</h2>
-            <Container>
-                <Row>
-                    <Col>
-                    <h3 className="social">Github</h3>
-                    <div className="social-links">
-                       <a className="social-link" href="/">Trung</a>
-                       <a className="social-link" href="/">Casey</a>
-                       <a className="social-link" href="/">Blake</a>
-                       <a className="social-link" href="/">Tim</a>
-                       </div>
-                    </Col>
-                </Row>
-          
-                <Row>
-                    <Col>
-                    <h3 className="social">LinkedIn</h3>
-                    <div className="social-links">
-                       <a className="social-link" href="/">Trung</a>
-                       <a className="social-link" href="/">Casey</a>
-                       <a className="social-link" href="/">Blake</a>
-                       <a className="social-link" href="/">Tim</a>
-                       </div>
-                    </Col>
-                </Row>
-            </Container>
-        </div> */}
