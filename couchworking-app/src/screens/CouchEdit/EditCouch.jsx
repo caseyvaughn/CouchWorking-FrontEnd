a0894fbbe502968d53f8165224fcd10adbb3fd9a
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-02-07 11:50:58 -0500

    fixed the edit couch form; now fully working

--- couchworking-app/src/screens/CouchEdit/EditCouch.jsx
+++ couchworking-app/src/screens/CouchEdit/EditCouch.jsx
@@ -26,25 +26,20 @@ export default function CouchEdit() {
           Authorization: `${localStorage.getItem("token")}`,
         },
       } );
-      // console.log(res)
-      // console.log(res.data);
       setInput(res.data);
     };
     fetchCouch();
   }, [id]);
 
   const handleSubmit = async (e) => {
-    // e.preventDefault();
+    e.preventDefault();
     const fields = input;
-    // await axios.put(`https://couch-working.herokuapp.com/couch-api/update/${id}`, fields);
-    const res = await axios.put(`https://couch-working.herokuapp.com/couch-api/update/${id}`, fields, {
+    await axios.put(`https://couch-working.herokuapp.com/couch-api/update/${id}`, fields, {
       headers: {
         Authorization: `${localStorage.getItem("token")}`,
       },
     })
-  
-    console.log(res.data)
-    // setInput(default_input);
+    setInput(default_input);
     navigate(`/couch/${id}`);
   };
 
@@ -56,15 +51,11 @@ export default function CouchEdit() {
     }));
   };
 
-  const routeCouchDetail = () => {
-    navigate(`/couch/${id}`)
-  }
-
   return (
     <div>
       <Display>
         {/* <h2>Thank you for updating your CouchWorking place!</h2> */}
-        <Form onSubmit={(e) => { handleSubmit(e) }}>
+        <Form onSubmit={handleSubmit}>
           <Form.Group>
             <Form.Label>Title</Form.Label>
             <Form.Control type="text" value={input.title} id='title' placeholder="Enter title" onChange={(e) => { handleTextInput(e) }} required/>
@@ -85,33 +76,10 @@ export default function CouchEdit() {
             <Form.Label>Contact Information</Form.Label>
             <Form.Control type="text" value={input.contactInfo} id='contactInfo' placeholder="Enter contact information" onChange={(e) => { handleTextInput(e) }}/>
           </Form.Group>
-          <Button variant="dark">Edit Couch</Button>
+          <Button variant="dark" onClick={handleSubmit}>Edit Couch</Button>
         </Form>
         </Display>
       </div>
   )
 }
 
-//Tim's original code
-// return (
-//   <div>
-//     <Display>
-//     <h2>Thank you for updating your CouchWorking place!!</h2>
-//       <form onSubmit={handleSubmit} id="postForm">
-//           <label htmlFor="title">Title</label>
-//           <input type="text" value={input.title} id='title' onChange={handleTextInput} required/><br />
-//           <label htmlFor="description">Description</label>
-//           <input type="text" value={input.description} id='description' onChange={handleTextInput}/><br />
-//           <label htmlFor="location">Location</label>
-//           <input type="text" value={input.location} id='location' onChange={handleTextInput} required/><br />
-//           <label htmlFor="image_URL">Image URL</label>
-//           <input type="text" value={input.image_URL} id='image_URL' onChange={handleTextInput}/><br />
-//           <label htmlFor="contactInfo">Contant info</label>
-//           <input type="text" value={input.contactInfo} id='contactInfo' onChange={handleTextInput}/><br />
-//       {/* <button onClick={routeCouchDetail}>Submit</button> */}
-//       <Button variant="dark" type="submit" size="lg" onClick={routeCouchDetail}></Button>
-//       </form>
-//       </Display>
-//     </div>
-// )
-// }
