1920a5520bd11b14fe22dbf28eb9377a99047c90
bweinmann <bsweinmann@gmail.com>
2022-02-07 11:58:05 -0500

    landing page scales correctly, footer not attatched to bottom on phone resolutions

--- couchworking-app/src/components/Footer/Footer.jsx
+++ couchworking-app/src/components/Footer/Footer.jsx
@@ -4,38 +4,36 @@ const Footer = () => {
 
     return (  
         <div className="main-footer">
-            <h4>Contact Us</h4>
-        <div className="container">
+          <h4>Contact Us</h4>
           <div className="row">
             <div className="col">
               <h4>LinkedIn</h4>
-              <ui className="list-unstyled">
+              <ul className="list-unstyled">
                     <a className="social-link" href="/">Trung</a>
                     <a className="social-link" href="/">Casey</a>
                     <a className="social-link" href="/">Blake</a>
                     <a className="social-link" href="/">Tim</a>
-              </ui>
+              </ul>
             </div>
             <div className="col">
               <h4>Github</h4>
-              <ui className="list-unstyled">
+              <ul className="list-unstyled">
                     <a className="social-link" href="/">Trung</a>
                     <a className="social-link" href="/">Casey</a>
                     <a className="social-link" href="/">Blake</a>
                     <a className="social-link" href="/">Tim</a>
-              </ui>
+              </ul>
             </div>
             <div className="col">
               <h4>Portfolio</h4>
-              <ui className="list-unstyled">
+              <ul className="list-unstyled">
                     <a className="social-link" href="/">Trung</a>
                     <a className="social-link" href="/">Casey</a>
                     <a className="social-link" href="/">Blake</a>
                     <a className="social-link" href="/">Tim</a>
-              </ui>
+              </ul>
             </div>
           </div>
-        </div>
       </div>
     )
 }
--- couchworking-app/src/components/Search/Search.jsx
+++ couchworking-app/src/components/Search/Search.jsx
@@ -1,18 +1,18 @@
-// import './Search.css'
+import './Search.css'
 
-// const Search = (props) => {
-//     return (
-//         <form className="search" onSubmit={(event) => props.onSubmit(event)}>
-//             <input
-//                 className="search-input"
-//                 value={props.value}
-//                 name="Search"
-//                 placeholder="Search"
-//                 type="text"
-//                 onChange={(event) => props.handleSearch(event)}
-//             />
-//         </form>
-//     )
-// }
+const Search = (props) => {
+    return (
+        <form className="search" onSubmit={(event) => props.onSubmit(event)}>
+            <input
+                className="search-input"
+                value={props.value}
+                name="Search"
+                placeholder="Search"
+                type="text"
+                onChange={(event) => props.handleSearch(event)}
+            />
+        </form>
+    )
+}
 
-// export default Search
+export default Search
