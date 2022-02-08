317f9d421870b3f010972d15808f1064bcf4bd36
Trung Diec <tdiec00@gmail.com>
2022-02-08 11:17:38 -0500

    loading page

--- couchworking-app/src/App.js
+++ couchworking-app/src/App.js
@@ -6,19 +6,46 @@ import UserSignUp from "./screens/UserSignUp/UserSignUp"
 import CouchList from "./screens/CouchList/CouchList"
 import CreateCouch from "./screens/CouchCreate/CreateCouch"
 import EditCouch from "./screens/CouchEdit/EditCouch"
+import {useState, useEffect} from "react"
+import {FillingBottle} from "react-cssfx-loading"
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 2000)
+  })
+
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/couch/:id" element={<CouchDetail />} />
-        <Route path="/update/:id" element={<EditCouch />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/sign-up" element={<UserSignUp />} />
-        <Route path="/couches" element={<CouchList />} />
-        <Route path="/create" element={<CreateCouch />} />
-      </Routes>
-    </div>
+    <>
+      {loading === false ? (
+        <div className="App">
+          <Routes>
+            <Route path="/couch/:id" element={<CouchDetail />} />
+            <Route path="/update/:id" element={<EditCouch />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/sign-up" element={<UserSignUp />} />
+            <Route path="/couches" element={<CouchList />} />
+            <Route path="/create" element={<CreateCouch />} />
+          </Routes>
+        </div>
+      ) : (
+        <div className="loading">
+          <h1>Page is loading...</h1>
+          <FillingBottle className="logo" />
+        </div>
+      )}
+    </>
+    // <div className="App">
+    //   <Routes>
+    //     <Route path="/couch/:id" element={<CouchDetail />} />
+    //     <Route path="/update/:id" element={<EditCouch />} />
+    //     <Route path="/" element={<HomePage />} />
+    //     <Route path="/sign-up" element={<UserSignUp />} />
+    //     <Route path="/couches" element={<CouchList />} />
+    //     <Route path="/create" element={<CreateCouch />} />
+    //   </Routes>
+    // </div>
   )
 }
 
